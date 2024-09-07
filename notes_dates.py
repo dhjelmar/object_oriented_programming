aa2c1a01ae8befcde5c5f4665f1a30d3c9453bc6
dlhjel <dlhjel@gmail.com>
2024-09-07 09:29:43 -0400

    added section on converting single values; prior had only looked at pandas columns

--- notes_dates.py
+++ notes_dates.py
@@ -1,9 +1,16 @@
+#%% [markdown]
+## STUDY ON HOW TO DEFINE AND COMPARE DATES
+
+#%% [markdown]
+## create pandas DataFrame with various format date columns
+
 #%%
-# importing pandas module
+# import pandas, datetime, and regex modules
 import pandas as pd
 import datetime as dt
+import regex as re
 
-# create pandas DataFrame with one column with five 
+#%%
 # datetime values through a dictionary
 df = pd.DataFrame({'YMDhms_str'       :               ['2021-01-15 00:00:00' , '1989-05-24 20:34:11'],
                    'YMD_str'          :               ['2021-01-15'          , '1989-05-24'         ],
@@ -34,12 +41,12 @@ df
 
 
 #%% [markdown]
-# COMPARE DATES
+## COMPARE DATES
 
 #%%
 # choose row to focus on
 irow = 1
-
+print('irow=', irow)
 
 #%%
 # following shows that: pd.Timestamp and dt.datetime are equivalent
@@ -54,10 +61,10 @@ def compare(df):
 compare(df)
 
 #%% [markdown]
-## LOOK AT DIFFERENT CONVERSIONS
+## LOOK AT DIFFERENT CONVERSIONS FOR PANDAS COLUMNS
 
 #%%
-# convert all columns to just date
+# convert all columns to just date using pd.to_datetime(df['date']).dt.date
 # following shows that: string times now eqivalent to dt.date
 #                       Timestamp and dt.datetime are still equiv but different from above
 dfc = df.copy()
@@ -70,7 +77,7 @@ compare(dfc)
 
 
 #%%
-# convert all columns to datetime
+# convert all columns to datetime using pd.to_datetime(dfc['date'])
 # following makes all dates in row 0 equivalent (and they should be)
 # following makes all dates in row 1 equivalent where they should be
 dfc = df.copy()
@@ -81,8 +88,84 @@ dfc
 print('for irow=', irow)
 compare(dfc)
 
+
+#%%
+## Convert pandas timestamp column to date string using df['date'].dt.strftime('%Y-%m-%d')
+#%%
+dfc=df.copy()
+dfc['datestring'] = dfc.YMDhms_timestamp.dt.strftime('%Y-%m-%d')
+dfc
+#%%
+print(type(dfc.datestring[0]))
+
+
+#%% [markdown]
+## CONVERT AND COMPARE SINGLE VALUE
+
+#%%
+YMDhms_str = '2021-01-15 11:09:08' 
+YMD_str = '2021-01-15' 
+#YMDhms_datetime = dt.datetime(YMDhms_str)
+#YMD_datetime = dt.datetime(YMD_str)
+
+def string2datetime(datetime_string):
+    # Convert string to datetime object
+    # If hour:minute:seconds are not provided, default is 00:00:00
+    if (re.search(":", datetime_string)):
+        format_string = '%Y-%m-%d %H:%M:%S'
+    else:
+        format_string = '%Y-%m-%d'
+    datetime_object = dt.datetime.strptime(datetime_string, format_string)
+    return datetime_object
+
+def datetime2string(date_object, dateonly=True):
+    # Convert the datetime object to a string in the specified format
+    if dateonly:
+        format_string = '%Y-%m-%d'
+    else:
+        format_string = '%Y-%m-%d %H:%M:%S'
+    datetime_str = date_object.strftime(format_string)
+    return datetime_str
+
+print('convert string in datetime format to datetime')
+mydate_obj = string2datetime(YMDhms_str)
+print(mydate_obj)
+print(type(mydate_obj))
+
+print()
+print('convert datetime to string')
+mydate_str = datetime2string(mydate_obj)
+print(mydate_str)
+print(type(mydate_str))
+
+print()
+print('convert datetime to string with hrs:min:sec')
+mydate_str = datetime2string(mydate_obj, dateonly=False)
+print(mydate_str)
+print(type(mydate_str))
+
+print()
+print('convert back to datetime')
+mydate_obj = string2datetime(mydate_str)
+print(mydate_obj)
+print(type(mydate_obj))
+
+print()
+print('convert string in date only format to datetime')
+mydate_obj = string2datetime(YMD_str)
+print(mydate_obj)
+print(type(mydate_obj))
+
+print()
+print('convert datetime to string')
+mydate_str = datetime2string(mydate_obj)
+print(mydate_str)
+print(type(mydate_str))
+
 #%% [markdown]
 ## CONCLUSION
 # The last example above using pd.to_datetime(dfc.iloc['date'], utc=True)
 # seems to behave most consistently. 
+
+# Created two functions to convert single values to datetime or string.
 #%%
